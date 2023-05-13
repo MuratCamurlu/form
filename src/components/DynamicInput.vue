1d2ff07d730c0262749048497ed8acac480a354a
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-05-13 12:10:34 +0300

    done

--- src/components/DynamicInput.vue
+++ src/components/DynamicInput.vue
@@ -3,11 +3,21 @@
     <div v-for="item in userExp" :key="item.company" style="display: flex">
       <div>
         <label for="name">Company Name</label>
-        <input type="text" id="name" placeholder="company A" />
+        <input
+          v-model="userExp.company"
+          type="text"
+          id="name"
+          placeholder="company A"
+        />
       </div>
       <div>
         <label for="exp">Experience</label>
-        <input type="number" id="exp" placeholder="Year" />
+        <input
+          v-model="userExp.exp"
+          type="number"
+          id="exp"
+          placeholder="Year"
+        />
       </div>
     </div>
   </div>
